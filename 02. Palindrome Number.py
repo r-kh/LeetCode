d688e50d403524d063d88dd09a5b971e3a4c8b7f
Rafael Khazivaliev <rafael.khazivaliev@gmail.com>
2025-07-08 04:35:09 +0300

    09. Palindrome Number

---
+++ 02. Palindrome Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x >= 0:
+            s = str(x)
+            if s[::-1] == s:
+                return True
+        return False
+
+
+s = Solution()
+result = s.isPalindrome(121)
+print(result)
